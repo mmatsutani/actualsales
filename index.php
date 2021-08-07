cbb4d7cd04575b6f6b1d7967546a9737bd0e1e29
Marcus Matsutani <matsutani@gmail.com>
2021-08-07 01:07:59 -0300

    Favicon added

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 	<title>HealthMax</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+	<!-- favicon -->
+	<link rel="icon" type="image/png" href="assets/img/favicon.png" />
+
 	<!-- css -->
 	<link href="assets/css/styles.css" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">

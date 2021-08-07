31761bb223e84c154c66b0b9cae408a68c5e2d42
Marcus Matsutani <matsutani@gmail.com>
2021-08-06 23:36:47 -0300

    Header added

---
+++ inc/header.php
@@ -0,0 +1,44 @@
+<header class="container-fluid">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 headline">
+				<img src="assets/img/logo-white.png" alt="HealthMax - Health Insurance" class="img-fluid">
+
+				<h1 class="py-md-0 py-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h1>
+
+				<ul class="py-md-0 py-5">
+					<li><i class="fas fa-star"></i> Lorem ipsum dolor sit amet, consectetur </li>
+					<li><i class="fas fa-star"></i> Lorem ipsum dolor sit amet, consectetur </li>
+					<li><i class="fas fa-star"></i> Lorem ipsum dolor sit amet, consectetur </li>
+				</ul>
+			</div>
+
+			<div class="col-lg-6 col-md-6">
+				<div class="form">
+					<form method="post" action="">
+						<div class="mb-4">
+							<input type="text" name="fullname" id="fullname" required="required" placeholder="Full name" class="form-control">
+						</div>
+
+						<div class="mb-4">
+							<input type="text" name="birth" id="birth" required="required" placeholder="Date of birth YYYY-MM-DD" pattern="\d{4}-\d{1,2}-\d{1,2}" class="form-control">
+						</div>
+
+						<div class="mb-4">
+							<input type="email" name="email" id="email" required="required" placeholder="E-mail" class="form-control">
+						</div>
+
+						<div class="mb-4">
+							<input type="text" name="phone" id="phone" required="required" placeholder="Phone number" pattern="/(7|8|9)\d{9}/" class="form-control">
+						</div>
+
+						<div class="mb-4">
+							<button type="submit" class="btn">Get Offer Now <i class="fas fa-angle-double-right"></i></button>
+						</div>
+					</form>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HealthMax</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- css -->
+	<link href="assets/css/styles.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	
+	<!-- js -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+	<!-- google fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+
+	<!-- font awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<!-- animate.style -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
+<body>
+<?php include('inc/header.php');?>
+</body>
+</html>
